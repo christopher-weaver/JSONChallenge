6e03a067477c0efa2e1fade959065480fdd3ad11
Christopher Weaver <christopher.m.weaver@outlook.com>
2021-07-27 21:33:23 -0400

    Add project files.

---
+++ JSONChallenge/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONChallenge
+{
+    public class Order : ObjectForJson
+    {
+        public string OrderId { get; set; }
+        public Customer Customer { get; set; }
+        public List<Product> LineItems { get; set; } = new List<Product>();
+        public DateTime PurchaseDate { get; set; }
+    }
+}
---
+++ JSONChallenge/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONChallenge
+{
+    public class Product
+    {
+        public string Sku { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice
+        { 
+            get
+            {
+                return Quantity * UnitPrice;
+            }
+        }
+    }
+}
